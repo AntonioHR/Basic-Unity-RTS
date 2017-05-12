fd0fca6602de46d2a138cde75fd3b381fb4dec9d
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2017-05-12 19:08:40 -0300

    Versão temporaria do Multi select

---
+++ Assets/Scripts/RTS/Mouse/RaycastHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace RTS.Mouse
+{
+    static class RaycastHelper
+    {
+        public static RaycastHit CastClickRay(LayerMask layers)
+        {
+            //Variável que vai guardar as informações de colisão do raio
+            RaycastHit hit;
+            //Posição atual do mouse
+            Vector3 MousePosition = Input.mousePosition;
+            //Raio a ser lançado
+            Ray r = Camera.main.ScreenPointToRay(MousePosition);
+
+            //Chamada do método
+            Physics.Raycast(r, out hit, float.PositiveInfinity, layers.value);
+
+            return hit;
+        }
+
+        public static RaycastHit[] BoxSelect(LayerMask layers, Vector3 startPosition)
+        {
+            float dist = Camera.main.nearClipPlane;
+            startPosition.z = dist;
+            var mousePos = Input.mousePosition;
+            mousePos.z = dist;
+            Vector3 startWorldPosition = Camera.main.ScreenToWorldPoint(startPosition);
+            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+            Quaternion cameraOrientation = Camera.main.transform.rotation;
+
+            Vector3 halfExtents = (mouseWorldPosition - startWorldPosition) / 2;
+            Vector3 center = startWorldPosition + halfExtents;
+            halfExtents = Quaternion.Inverse(cameraOrientation) *  halfExtents;
+            halfExtents.z = 1;
+            //ExtDebug.DrawBoxCastBox(center,
+            //    halfExtents, cameraOrientation, cameraOrientation * Vector3.forward, 100, Color.red);
+
+
+            return Physics.BoxCastAll(center, 
+                halfExtents, cameraOrientation * Vector3.forward, cameraOrientation, 100);
+        }
+    }
+}
---
+++ Assets/Scripts/RTS/Unit.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace RTS
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Unit : MonoBehaviour
+    {
+        public Material idle;
+        public Material selected;
+
+        MeshRenderer meshRenderer;
+        NavMeshAgent navMeshAgent;
+
+
+        public bool Selectable
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            meshRenderer = GetComponent<MeshRenderer>();
+            meshRenderer.material = idle;
+        }
+        void Update()
+        {
+
+        }
+
+
+
+        public void Deselect()
+        {
+            meshRenderer.material = idle;
+        }
+        public void Select()
+        {
+            meshRenderer.material = selected;
+        }
+
+        public void Target(GameObject target, Vector3 position)
+        {
+            Ground g = target.GetComponent<Ground>();
+            if (g != null)
+            {
+                navMeshAgent.destination = position;
+            }
+        }
+
+    }
+}
