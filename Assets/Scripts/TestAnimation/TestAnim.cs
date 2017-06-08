b9f6b8e27e7eba8fbaae0d77ade91df43d2135bb
Daniel de Souza Queiroga <dsq@WINDOWS.CIN.UFPE.BR>
2017-06-08 15:58:58 -0300

    mock up da animação

---
+++ Assets/Scripts/TestAnimation/TestAnim.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnim : MonoBehaviour {
+
+	public Animator animator;
+	public bool dead, walk, damage, selected = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("q")) 
+		{
+			selected = !selected;
+		}
+		if (Input.GetKeyDown ("w")) 
+		{
+			dead = !dead;
+		}
+		if (Input.GetKeyDown ("e")) 
+		{
+			walk = !walk;
+		}
+		if (Input.GetKeyDown ("r")) 
+		{
+			damage = !damage;
+		}
+	}
+
+	void LateUpdate() {
+		animator.SetBool("Selected", selected);
+		animator.SetBool("Dead", dead);
+		animator.SetBool("Walk", walk);
+		animator.SetBool("TookDamage", selected);
+	}
+}
