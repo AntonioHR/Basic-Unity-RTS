38d6ca7da4c5cc325d53a47e97b1e03a26d70c21
dansq <dsq@cin.ufpe.br>
2017-06-17 16:31:41 -0300

    atualizando mais uma vez, animações começando a ser integradas

---
+++ Assets/Scripts/RTS/World/Unit/LightInfantryAnimationHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTS.World.Units
+{
+    [RequireComponent(typeof(Animator))]
+	public class LightInfantryAnimationHandler : MonoBehaviour {
+        Animator animator;
+
+        public event System.Action OnHitFrame;
+
+        void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void HitFrame()
+        {
+            if (OnHitFrame != null)
+                OnHitFrame();
+        }
+
+        internal void SetAttacking(bool value)
+        {
+			animator.SetBool("Attack", value);
+        }
+
+		internal void SetSelected(bool value)
+		{
+			animator.SetBool("Selected", value);
+		}
+
+		internal void SetWalking(bool value)
+		{
+			animator.SetBool("Walking", value);
+		}
+    }
+}
---
+++ Assets/Scripts/RTS/World/UnitExtendHOOOOOOOO.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using RTS.World.Units;
+
+
+namespace RTS.World
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+	public class UnitExtendHOOOOOOOO : MonoBehaviour, ISelectable, IHighlightable, IHittable, ITargetReceiver, IInteractive
+    {
+        [System.Serializable]
+        public class Settings
+        {
+            public float range;
+            public float attackRangeTolerance = .1f;
+            public int damage;
+            public Material idleMaterial;
+            public Material selectedMaterial;
+        }
+
+
+
+        public Settings settings;
+
+        public Transform selectionIndicator;
+        //public MeshRenderer meshRenderer;
+		public LightInfantryAnimationHandler animationHandler;
+		public Transform unitTransform;
+		private Vector3 curPos;
+		private Vector3 oldPos;
+
+        public event System.Action OnDestroyed;
+
+        NavMeshAgent navMeshAgent;
+        IHittable hitTarget;
+
+        
+
+        public bool Selectable { get { return true; } }
+        public bool Highlightable { get { return true; } }
+        public bool CanTarget { get { return true; } }
+        public bool Targetable { get { return true; } }
+        public bool Hittable { get { return true; } }
+        public GameObject Owner { get { return gameObject; } }
+        public Vector3 position { get { return transform.position; } }
+        public bool IsInRange
+        {
+            get
+            {
+                if (hitTarget == null)
+                    return false;
+                Vector3 realDistance = hitTarget.position;
+                realDistance.y = transform.position.y;
+                return (Vector3.Distance(realDistance, transform.position) - settings.attackRangeTolerance) < settings.range;
+            }
+        }
+
+
+
+
+        void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+           // meshRenderer = meshRenderer != null? meshRenderer: GetComponent<MeshRenderer>();
+           // meshRenderer.material = settings.idleMaterial;
+
+			animationHandler.GetComponent<LightInfantryAnimationHandler>();
+            animationHandler.OnHitFrame += HitCurrentTarget;
+			curPos = new Vector3 (unitTransform.position.x, unitTransform.position.y, unitTransform.position.z);
+			oldPos = curPos;
+        }
+        void Update()
+        {
+            var inrange = IsInRange;
+            if (inrange)
+            {
+                Vector3 lookTarget = hitTarget.position;
+                lookTarget.y = transform.position.y;
+                transform.LookAt(lookTarget);
+            }
+            animationHandler.SetAttacking(inrange);
+
+			animationHandler.SetWalking(false);
+			curPos = unitTransform.position;
+			string curpos = "curPos: " + curPos.ToString();
+			string oldpos = "oldPos: " + oldPos.ToString();
+			Debug.Log(curpos);
+			Debug.Log (oldpos);
+			if(curPos != oldPos)
+			{
+				animationHandler.SetWalking(true);
+			}
+
+			oldPos = curPos;
+        }
+        public void OnDestroy()
+        {
+            if (OnDestroyed != null)
+                OnDestroyed();
+        }
+
+
+
+        public void Deselect()
+        {
+			animationHandler.SetSelected (false);
+        }
+        public void Select()
+        {
+			animationHandler.SetSelected (true);
+        }
+        
+        public void HighlightOn()
+        {
+            //meshRenderer.material = settings.selectedMaterial;
+        }
+        public void HighlightOff()
+        {
+            //meshRenderer.material = settings.idleMaterial; 
+        }
+
+        public void TargetBy(ITargetReceiver targetReceiver)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetTarget(ITargetable target, Vector3 position)
+        {
+            if (target != null)
+            {
+                navMeshAgent.stoppingDistance = 0;
+                navMeshAgent.destination = position;
+                var hittable = target as IHittable;
+                setHitTarget(hittable);
+                if (hittable != null)
+                {
+                    navMeshAgent.stoppingDistance = settings.range;
+                }
+            }
+            else
+                setHitTarget(null);
+        }
+
+        public void HitCurrentTarget()
+        {
+            if(hitTarget!= null)
+                hitTarget.Hit(settings.damage);
+        }
+
+        public void Hit(int damage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+
+        void clearHitTarget()
+        {
+            setHitTarget(null);
+        }
+        void setHitTarget(IHittable target)
+        {
+            if (hitTarget != null)
+                hitTarget.OnDestroyed -= clearHitTarget;
+            if (target != null)
+                target.OnDestroyed += clearHitTarget;
+            hitTarget = target;
+        }
+    }
+}
